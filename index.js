47ae415562cf264b6c670f0ef428ad683c7b563c
rinotoharto <rinotoharto@gmail.com>
2020-03-30 19:31:08 +0700

    check vowels done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function checkVowel(string) {
+    var vokal = 'aiueo'
+    var temp = ''
 
+    for(let i = 0; i < string.length; i++) {
+        for(let j = 0; j < vokal.length; j++) {
+            if(string[i] == vokal[j]) {
+                temp += string[i]
+            }
+        }
+    }
+
+    if(temp.length == string.length) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(checkVowel('rinotoharto'));
+console.log(checkVowel('rabsnckksola'));
+console.log(checkVowel('aiueoaiueoaiuoe'));
+console.log(checkVowel('aaaaaaaaaaadaaaa'))
