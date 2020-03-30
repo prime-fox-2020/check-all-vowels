6cd7e73480ddeebedde44996ba723709203c97ab
charles2901 <chrlsjnthn@gmail.com>
2020-03-30 14:49:29 +0700

    solve

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+function checkVowel(str){
+    let vowel = 'aiueo'
+    let flag = false
+    for(let i = 0; i < vowel.length; i++){
+        flag = false
+        for(let j = 0; j < str.length; j++){
+            if(vowel[i] == str[j]){
+                flag = true
+            }
+        }
+        if(!flag){
+            break
+        }
+    }
+    return flag
+}
+console.log(checkVowel('aiueo')) // true
+console.log(checkVowel('adfghjuklmikjkjemlkjhdo')) //true
+console.log(checkVowel('baneikk')) //false
