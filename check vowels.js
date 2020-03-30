be8e9ff74e007220f3be753e4e5fb091b26cbaa6
susilo hartomo <encusilo545110@gmail.com>
2020-03-30 12:49:27 +0700

    susilo_hartomo

---
+++ check vowels.js
@@ -0,0 +1,19 @@
+// function didalam string semua huruf vokal atau tidak
+// bila semua huruf vocal maka true
+// bila semua bukan huruf vocal maka false
+function checkVowels(str) {
+    const kamus = "aiueo AIUEO"
+    let check = true
+    for (let i = 0; i < str.length; i++) {
+        if (!kamus.includes(str[i])) {
+            console.log(str[i]);
+            check = false
+            return check
+        }
+    }
+    return check
+}
+
+console.log(checkVowels("Au kSt"));
+console.log(checkVowels("AiEo U"));
+console.log(checkVowels("AIUeo"));
