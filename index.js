ac9e8dab36a5333a7e7cb56885d3dd12188b2979
rakdhan <rakahamijoyo@gmail.com>
2020-04-01 01:59:04 +0700

    yeah-checkVows

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+function checkVowels(str) {
+  var count = 0
+  var dic = 'AIUEOaiueo'
+  for (let i in str) {
+    if (str[i] === ' ') count += 1;
+    for (let j in dic) {
+      if (str[i] === dic[j]) count += 1;
+    }  
+  } return count == str.length ? true : false
+}
+
+console.log(checkVowels("EeEO")); //true
+console.log(checkVowels("Ia ioo UaAa")); //true
+console.log(checkVowels("Raka Dhaneswar4")); //false
