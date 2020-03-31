2f809fccc75ec414f19268d5ccd09b28f4d92c0f
Vincent Guizot <30890039+CenZo128@users.noreply.github.com>
2020-03-31 10:35:09 +0700

    Fixed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+let checkAllVowel = (str) => {
+    let count=0
+    str = str.toUpperCase()
 
+        for(let j=0;j<str.length;j++){
+            if(checkVowels(str[j])==true){
+                count++
+            }
+    }
+    return (count==str.length) ? true : false
+}
+let checkVowels = (x) => {
+    let vowels = ['A','I','U','E','O'], check
+    for(let i=0;i<vowels.length;i++){
+        if(x==vowels[i]) check=true 
+    }
+    return (check) ? true : false
+}
+console.log(checkAllVowel("AAA"))
