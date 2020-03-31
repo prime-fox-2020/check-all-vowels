e8ce8deee563fcdbae05859540aac382c034e478
akmaliavitri <akmaliavitri21@gmail.com>
2020-03-31 19:11:48 +0700

    aiueo

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function vowels(str){
 
+    let strLow = str.toLowerCase()
+    //console.log(strLow)
+    let vocal = 'aiueo'
+    let result = ''
+    for(let i=0; i<strLow.length; i++){
+        //console.log(strLow[i])
+        for(let j=0; j<vocal.length; j++){
+            if(strLow[i] == vocal[j] ){
+                result += strLow[i]
+            }
+        }
+    }
+
+    if(result.length == str.length) return true
+    return false
+}
+console.log(vowels('AauIUEO'))
+console.log(vowels('AKYEO'))
