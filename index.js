62518eadb4b84f24064d995f5b2228382fe8db4f
Mercen1 <nurhabib102@gmail.com>
2020-04-01 22:56:02 +0700

    Release0: checkAllVowels Solve

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+function checkAllVowels(str){
+    const vowels = 'AIUEOaiueo'
+    const konsonanKecil = 'aiueo'
+    for (let i = 0; i < str.length; i++) {
+        var cek = false;
+        for (let j  = 0; j < vowels.length; j++) {
+            if(str[i]==vowels[j]){
+                cek = true
+            }
+        }
+        if(cek==false){
+            return cek
+        }
+    }
+    return cek
+}
+
+
+console.log(checkAllVowels('AiueiEiouoi')) // true
+console.log(checkAllVowels('aaeutyksmhJ')) // false
+console.log(checkAllVowels('AkuSukaMakan')) // false
+console.log(checkAllVowels('AUIEUEIUEA')) // true
+console.log(checkAllVowels('aeieuiuiao')) // true
