c69fd9ba32585ea56cbc6e1b35ef8a0fd79f1847
bzxnaga <bzxnaga@gmail.com>
2020-04-01 19:39:07 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+function checkVowels(x){
+    let vowels = ['a','i','u','e','o']
+    let check;
+    for(let i = 0 ;i < vowels.length ; i++){
+        if(x == vowels[i]){
+            check=true
+        } 
+    }
+    return check
+}
 
+function checkAllVowel (str){
+    let count = 0
+    str = str.toLowerCase()
+
+    for(let i = 0 ;i < str.length ; i++){
+        if(checkVowels(str[i])==true){
+            count++
+        }
+    }
+
+    if(count == str.length){
+        return true
+    }else{
+        return false
+    }
+
+}
+
+console.log(checkAllVowel('AauIUEO'))
+console.log(checkAllVowel('AKYEO'))
+console.log(checkAllVowel('YibUYurYTIB1927&(@*89AE'))
+console.log(checkAllVowel('AAAAAAAAAAAAAAAAAIIIIIIIIIIIIUUUUUUUUEO'))
