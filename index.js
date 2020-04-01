337df83ffaef0c325d06916ddf6252ebee3f1964
Rozaan3309 <Rozaan3309@users.noreply.github.com>
2020-04-01 13:48:29 +0700

    check all vowels release 1 done

--- index.js
+++ index.js
@@ -15,9 +15,53 @@ function generateArr(row, col){
         }
         arr.push(temp);
     }
-
     return arr
 
 }
 
-console.log(generateArr(5, 4));
+function getVowels(arr){
+    let list1 = ['A', 'I', 'U', 'E', 'O', ];
+    let checker = 0
+    let flag = true
+    let getDown = 0
+    while(flag === true && getDown < (arr.length - 1)){
+        let valBox = []
+        for(let b = 0; b < arr[0].length - 1; b++){
+            let pusher = []
+            pusher.push(arr[getDown][b])
+            pusher.push(arr[getDown][b+1])
+            valBox.push(pusher)
+            pusher = []
+            pusher.push(arr[getDown+1][b])
+            pusher.push(arr[getDown+1][b+1])
+            valBox.push(pusher)
+
+            let flag2 = true
+            let validate = 0
+            for(let c = 0; c < valBox.length; c++){
+                for(let d = 0; d < valBox[c].length; d++){
+                    for(let e = 0; e < list1.length; e++){
+                        if(valBox[c][d] === list1[e]){
+                            validate++
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (validate === 4){
+                checker++
+                valBox = []
+            } else {
+                valBox = []
+            }
+        }
+        getDown++
+    }
+
+    return checker
+}
+
+
+
+console.log(getVowels(generateArr(5, 4)))
