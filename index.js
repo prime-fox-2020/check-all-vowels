113d82ae4229a7150d1ae1af2e20f8e09560084b
elvanrafif <terlanjurbuta@gmail.com>
2020-04-05 16:28:48 +0700

    add tugas additional check all vowels

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+
+function apakahVokalSemua(str){
+  let kamus = 'AIUEOaiueo'
+
+  var res = 0
+  for (let i = 0; i < str.length; i++){
+    for (let j = 0; j < kamus.length; j++){
+      if (str[i] == kamus[j]){
+        res += 1
+      }
+    }
+  }
+  if (res == str.length){
+    return true
+  } else 
+    return false
+}
+
+console.log(apakahVokalSemua('AUEOEUIO'));
+console.log(apakahVokalSemua('JAKARTA'));
+console.log(apakahVokalSemua('AIOEUER'));
