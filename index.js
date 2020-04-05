1d94f8943f7f94ed7295523e7d7e7ca7878c65ee
vinita-dewi <61119004+vinita-dewi@users.noreply.github.com>
2020-04-06 00:06:48 +0700

    finished checkvowels

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function checkVowels(str){
+    let alphabet ='aeiou'
+    let count = 0
+    for(var i =0; i< str.length; i++){
+        for(var j=0;j<alphabet.length; j++){
+            if(str[i] == alphabet[j]){
+                count++
+            }
+        }
+    }
+    if(count == str.length){
+        return true 
+    }else{
+        return false
+    }
+}
+
+console.log(checkVowels('aeoiou'))
+console.log(checkVowels('kjlmnq'))
+console.log(checkVowels('abcdefg'))
