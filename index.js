d0c6804ffb613741e1cf38fc4aa18e93bbe1b7d9
Sujackson <su_jackson99@yahoo.com>
2020-04-05 16:37:49 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+let vokal='aeiou'
 
+function check(kalimat){
+    for (let i = 0; i < kalimat.length; i++) {
+        for (let j = 0; j < vokal.length; j++) {
+            if(kalimat[i]==vokal[j]){
+                return true
+            }
+        }
+    }
+    return false
+}
+
+console.log(check('sdqccaaa')) //true
+console.log(check('aiueo')) // true
+console.log(check('hjgnvyd')); // false
