4e408eb07e2800888ea997c68e2ca7f4ba806812
fahrulnoer <fahrulnoer17@gmail.com>
2020-04-05 12:10:47 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+function checkVowels(str) {
+    const vowels = 'AIUEOaiueo';
+    let check = false
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < vowels.length; j++) {
+            if (str[i] === vowels[j]) {
+                return true
+            }
+        }
+    }
+    return check
+}
+console.log(checkVowels('asdf'))
