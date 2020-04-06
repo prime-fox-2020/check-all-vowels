2ccf88f3aec29511f7f275c967de9fdd633ccbbc
permaficus <abukhalif2019@gmail.com>
2020-04-06 22:57:27 +0700

    Check all vowels done !

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+checkAllVowels = string => {
+
+    const vowels = 'aiueo';
+    let counter = 0;
+    for (i of string) {
+        if (vowels.indexOf(i) !== -1) {
+            counter++
+        }
+    }
+
+    return (string.length == counter)
+}
+
+// test case
+console.log(checkAllVowels('aiueo'))
+console.log(checkAllVowels('hacktiv8'))
+console.log(checkAllVowels('uuuuuuuuuuuu'))
+console.log(checkAllVowels('rcbdkl'))
