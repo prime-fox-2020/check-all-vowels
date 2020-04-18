53f3a39dfa41a0f3eafe771a0732c95945c23f2f
aireljordan98 <joeyjordanwilliams@gmail.com>
2020-04-18 14:06:45 +0700

    airell

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+function checkVowels(str){
+const vowels = 'aeiou'
+let counter = 0;
+
+
+for(let i = 0; i < str.length; i++){
+  for(let j = 0; j < vowels.length; j++){
+    if(str[i].toLowerCase() === vowels[j]){
+      counter++
+    }
+  }
+}
+
+if(counter === str.length){
+  return true;
+}else{
+  return false;
+}
+}
+
+
+console.log(checkVowels('oooo'))
+console.log(checkVowels('adkfndoknei'))
