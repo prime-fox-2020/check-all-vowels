472b7fd0bf2dededcbf5747c0c56bee49a12d6aa
unknown <sonymartha4@gmail.com>
2020-04-19 17:46:57 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function checkVowels(str){
+    let cek ='aeiou'
+    let count = 0
+    for(var i =0; i< str.length; i++){
+        for(var j=0;j<cek.length; j++){
+            if(str[i] == cek[j]){
+                count++
+            }
+        }
+    }
+    if(count == str.length){
+        return true 
+    }else{
+        return false
+    }
+}
 
+console.log(checkVowels('aeoiou'))
+console.log(checkVowels('kjlmnq'))
+console.log(checkVowels('abcdefg'))
