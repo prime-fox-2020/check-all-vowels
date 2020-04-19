9a21145542df378565762d8fd2ff239debf7515f
pippop21 <afifar38@gmail.com>
2020-04-19 21:18:25 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function checkAllVowels(string){
+  const dictionary = 'aiueoAIUEO'
+  for(let str of string){
+    for(let dict of dictionary){
+      if(str === dict) return true
+    }
+  }
+  return false
+}
 
+console.log(checkAllVowels('Hello World'))
+console.log(checkAllVowels('CB DRMN'))
