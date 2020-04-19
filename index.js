653d92d75700e791f8ffb17488ce322a19be4cbd
michaelm96 <michaelarceus96@gmail.com>
2020-04-19 22:35:39 +0700

    update ADDITIONAL check all vowels

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function checkVowel(str){
+    let vowel = "AEIUOaeiuo"
+    for (let i = 0; i < str.length; i++) {
+        if(vowel.indexOf(str[i]) == -1){
+            return false
+        }
+    }
+    return true
+}
 
+console.log(checkVowel("aeuio"))
+console.log(checkVowel("aesio"))
+console.log(checkVowel("ayuio"))
+console.log(checkVowel("qwrty"))
