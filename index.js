bb938af7e44332e668a28fd749c93fa622a5a6b6
Zetra416 <penguin_vs_yeti@yahoo.com>
2020-04-19 18:27:23 +0700

    finished checkAllVowels

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function checkAllVowels(str){
+    let vowel ='aiueo';
+    let count = 0;
 
+    for(let i = 0; i< str.length; i++){
+        for(let j= 0;j<vowel.length; j++){
+            if(str[i] == vowel[j]){
+                count++;
+            }
+        }
+    }
+    if(count === str.length){
+        return true
+    }else{
+        return false
+    }
+}
+
+//test case
+console.log(checkAllVowels('aiueo')) //true
+console.log(checkAllVowels('kjlmnqaiueo')) //false
+console.log(checkAllVowels('nfdtphjx')) //false
