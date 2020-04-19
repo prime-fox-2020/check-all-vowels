3a6f711669eecdb9929c5ec0f0135a3e8bfbeafa
Adib Faqih <foezst@gmail.com>
2020-04-19 15:49:05 +0700

    release 0

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const text = 'oooiii';
+const text2 = 'ooaoiiuoaiuda';
 
+function checkVokal(text) {
+  const vokal = 'aiueo';
+  for (let i = 0; i < text.length; i++) {
+    let flag = false;
+    for (let j = 0; j < vokal.length; j++) {
+      if (text[i] == vokal[j]) {
+        flag = true;
+      }
+    }
+    if (!flag) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(checkVokal(text));
+console.log(checkVokal(text2));
