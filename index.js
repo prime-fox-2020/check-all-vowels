03fc214e67f3d00f1b91d74b73a9a8f5f3af0794
zuhdi <zudhy.duddy@gmail.com>
2020-04-20 13:01:25 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function checkAllVowels(str) {
+  var vowels = 'aiueo'
+  var temp = ''
 
+  for(let i = 0; i < str.length; i++) {
+      for(let j = 0; j < vowels.length; j++) {
+          if(str[i] == vowels[j]) {
+              temp += str[i]
+          }
+      }
+  }
+  console.log(temp);
+  console.log(str.length);
+
+  if(temp.length == str.length) {
+      return true
+  } else {
+      return false
+  }
+}
+
+console.log(checkAllVowels('abcdefghijklmnopqrstuvwxyz'))
+console.log(checkAllVowels('bcdfghjklmnpqrstvwxyz'))
+console.log(checkAllVowels('jlksdjfs'))
+console.log(checkAllVowels('aiueoo'))
+console.log(checkAllVowels('oeuia'))
+console.log(checkAllVowels('aaaaaaaaaaaaaaaaaaaaaaaaaaaaab'))
